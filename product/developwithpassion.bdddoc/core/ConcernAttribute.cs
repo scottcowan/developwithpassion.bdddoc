bb65d74486c323461621148a2ce01521c4c89104
Scott Cowan <info@scottcowan.org>
2009-04-23 22:02:14 +0100

    tag Concerns with the story they relate to ie [Concern(typeof(FooClass),"TAG-7")]

--- product/developwithpassion.bdddoc/core/ConcernAttribute.cs
+++ product/developwithpassion.bdddoc/core/ConcernAttribute.cs
@@ -5,11 +5,15 @@ namespace developwithpassion.bdddoc.core
     [AttributeUsage(AttributeTargets.Class)]
     public class ConcernAttribute : Attribute
     {
+        public string story_key { get; private set; }
         public Type concerned_with { get; private set; }
 
-        public ConcernAttribute(Type concern)
+        public ConcernAttribute(Type concern) : this(concern,"") { }
+
+        public ConcernAttribute(Type concern, string story_key)
         {
             this.concerned_with = concern;
+            this.story_key = story_key;
         }
     }
 }
